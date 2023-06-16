490204337d8f4b87c2086e8754e00291d496f7ec
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-16 12:12:10 +0200

    Added: 67, 83 solutions

---
+++ solutions/0000-0099/83.Remove Duplicates from Sorted List/solution.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (!head) return null
+
+  while (head.next && head.val == head.next.val) {
+    head.next = head.next.next
+  }
+  head.next = deleteDuplicates(head.next)
+  return head
+}
