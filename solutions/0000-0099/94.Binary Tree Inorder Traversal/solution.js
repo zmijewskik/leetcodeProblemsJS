895ff2d66e00b59354404002baeae78ebf4f0612
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-07-19 23:49:25 +0200

    Added: 94 solution

---
+++ solutions/0000-0099/94.Binary Tree Inorder Traversal/solution.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const stack = []
+  const res = []
+
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root)
+      root = root.left
+    } else {
+      root = stack.pop()
+      res.push(root.val)
+      root = root.right
+    }
+  }
+  return res
+}
+
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
