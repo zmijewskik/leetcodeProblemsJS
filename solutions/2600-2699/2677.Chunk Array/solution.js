bd4e13b856725a9a4acf2a3e7f6341f5cd963215
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-08-30 21:26:32 +0200

    Added: 2637

--- solutions/2600-2699/2677.Chunk Array/solution.js
+++ solutions/2600-2699/2677.Chunk Array/solution.js
@@ -12,3 +12,47 @@ const chunk = (arr, size) => {
 
   return chunkedArray
 }
+
+// /**
+//  * @param {Array} arr
+//  * @param {number} size
+//  * @return {Array[]}
+//  */
+// let chunk= (arr, size) =>{
+//     const chunkedArray = [];
+//     let index = 0;
+
+//     while (index < arr.length) {
+//       chunkedArray.push(arr.slice(index, index + size));
+//       index += size;
+//     }
+
+//     return chunkedArray;
+//   }
+
+// /**
+//  * @param {Array} arr
+//  * @param {number} size
+//  * @return {Array[]}
+//  */
+// const chunk = (arr, size) =>
+//   Array.from({ length: Math.ceil(arr.length / size) }, (_, index) =>
+//     arr.slice(index * size, index * size + size)
+//   );
+
+// /**
+//  * @param {Array} arr
+//  * @param {number} size
+//  * @return {Array[]}
+//  */
+// const chunk = (arr, size) => {
+//     return arr.reduce((chunkedArray, element) => {
+//       const lastChunk = chunkedArray[chunkedArray.length - 1];
+//       if (!lastChunk || lastChunk.length === size) {
+//         chunkedArray.push([element]);
+//       } else {
+//         lastChunk.push(element);
+//       }
+//       return chunkedArray;
+//     }, []);
+//   };
