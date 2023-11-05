db13c93c157345621043466f5454b6c16abae61c
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-11-05 22:13:31 +0100

    Added: 111 + comments

---
+++ solutions/0100-0199/111.Minimum Depth of Binary Tree/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+  // If the subtree is empty i.e. root is NULL, return depth as 0...
+  if (root == null) return 0
+
+  // If the both subtrees are empty...
+  if (root.left == null && root.right == null) return 1
+
+  // If the left subtree is empty, return the depth of right subtree after adding 1 to it...
+  if (root.left == null) return 1 + minDepth(root.right)
+
+  // If the right subtree is empty, return the depth of left subtree after adding 1 to it...
+  if (root.right == null) return 1 + minDepth(root.left)
+
+  // When the two child function return its depth...
+  // Pick the minimum out of these two subtrees and return this value after adding 1 to it...
+  // Adding 1 is the current node which is the parent of the two subtrees...
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+}
