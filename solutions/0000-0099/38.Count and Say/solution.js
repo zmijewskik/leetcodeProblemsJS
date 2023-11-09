db5a2b46720a76dcf9adc582e120c6d9850ba77c
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-11-09 23:17:17 +0100

    Added: 110

--- solutions/0000-0099/38.Count and Say/solution.js
+++ solutions/0000-0099/38.Count and Say/solution.js
@@ -22,3 +22,18 @@ var countAndSay = function (n) {
   }
   return str
 }
+
+// Examples of nth sequence
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+// 6.     312211
+// 7.     13112221
+// 8.     1113213211
+// 9.     31131211131221
+// 10.    13211311123113112211
+// (i+1)th sequence is the "count and say" of the ith sequence
+// next sequence is describing the last one
+// look at digits as pairs fe. 11 12 21 -> 111221 in 5th
